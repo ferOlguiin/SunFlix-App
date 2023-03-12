fe78914e369d873c907d328cecf54efa60551909
ferOlguiin <feergui997@hotmail.com>
2023-03-12 16:16:01 -0300

    client-api-urlAdd

--- client/src/api/backendConnection.js
+++ client/src/api/backendConnection.js
@@ -1,22 +1,23 @@
 import axios from "axios";
+const url = process.env.REACT_APP_API_BASE_URL;
 
 //PARA SERIES
-export const getSeriesRequest = async () => await axios.get("/series");
+export const getSeriesRequest = async () => await axios.get(`${url}/series`);
 
-export const getOneSerieRequest = async (id) => await axios.get("/serie/" + id);
+export const getOneSerieRequest = async (id) => await axios.get(`${url}/serie/${id}`);
 
-export const editSerieRequest = async (id, newFields) => axios.put("/editserie/" + id, newFields);
+export const editSerieRequest = async (id, newFields) => axios.put(`${url}/editserie/${id}`, newFields);
 
 
 //PARA PELICULAS    
-export const getMoviesRequest = async () => await axios.get("/movies");
-export const getOneMovieRequest = async (id) => await axios.get("/getonemovie/" + id);
-export const editMovieRequest = async (id, newFields) => await axios.put("/editmovie/" + id, newFields);
+export const getMoviesRequest = async () => await axios.get(`${url}/movies`);
+export const getOneMovieRequest = async (id) => await axios.get(`${url}/getonemovie/${id}`);
+export const editMovieRequest = async (id, newFields) => await axios.put(`${url}/editmovie/${id}`, newFields);
 
 
 //PARA USUARIOS 
-export const getUserRequest = async (fields) => await axios.post("/user", fields);
+export const getUserRequest = async (fields) => await axios.post(`${url}/user`, fields);
 
-export const createUserRequest = async (newFields) => await axios.post("/createuser", newFields);
+export const createUserRequest = async (newFields) => await axios.post(`${url}/createuser`, newFields);
 
-export const editUserRequest = async (id, data) => await axios.put("/edituser/" + id, data);
+export const editUserRequest = async (id, data) => await axios.put(`${url}/edituser/${id}`, data);
